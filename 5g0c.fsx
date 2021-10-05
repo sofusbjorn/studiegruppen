14aa82807949b757b84ce2e9ddd7fa9600d0a6ec
Sofus Bjørn <sofusostrowska@gmail.com>
2021-10-05 11:24:32 +0200

    new files

--- 5g0c.fsx
+++ 5g0c.fsx
@@ -3,8 +3,7 @@ let dropFirstColumn (llst : 'a list list) : 'a list list =
     if notEmpty = true then
         List.map ( fun (xs : 'a list ) -> xs.Tail ) llst
     else 
-        let sorryBro = [[4];[0];[4]]
-        sorryBro
+        []
 
 printfn "%A" (dropFirstColumn [[1;2;3];[1;2;3];[1;2;3]])
 printfn "%A" (dropFirstColumn [[];[]])
